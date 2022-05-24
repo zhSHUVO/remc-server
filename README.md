faf1849f41aecb5c5a77e002b5ebc5260b7ef9ea
zhSHUVO <zhshuvo99@gmail.com>
2022-05-24 19:22:12 +0600

    collection setup

---
+++ README.md
@@ -0,0 +1,3 @@
+# REMC - Root Electronics and Manufacturing Company
+
+Biggest Basic Electronics Manufacturing Company in the world, come visit us. Maybe you'll find what you need. Our website [REMC](https://github.com/facebook/create-react-app).
--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const port = process.env.PORT || 5000;
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 
 app.use(cors());
 app.use(express.json());
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.sl9pe.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    serverApi: ServerApiVersion.v1,
+});
+
+async function run() {
+    try {
+        await client.connect();
+        const electronicsCollection = client
+            .db("remc-database")
+            .collection("electronics");
+
+        const ordersCollection = client
+            .db("remc-database")
+            .collection("orders");
+
+        
+        });
+    } finally {
+    }
+}
+
+run().catch(console.dir);
 
 app.get("/", (req, res) => {
     res.send("REMC data server is running");
