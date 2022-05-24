c8d60e7736a19967b43f7e3b2eebe50e86b07f34
zhSHUVO <zhshuvo99@gmail.com>
2022-05-24 19:24:11 +0600

    load one

--- index.js
+++ index.js
@@ -34,6 +34,14 @@ async function run() {
             const electronics = await cursor.toArray();
             res.send(electronics);
         });
+
+        // load one electronics
+        app.get("/product/:id", async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const electronic = await electronicsCollection.findOne(query);
+            res.send(electronic);
+        });
     } finally {
     }
 }
