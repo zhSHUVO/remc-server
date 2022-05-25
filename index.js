c42664549f42889c70282211c236e0b7ea2ae11e
zhSHUVO <zhshuvo99@gmail.com>
2022-05-25 21:56:41 +0600

    protech admin access

--- index.js
+++ index.js
@@ -68,12 +68,31 @@ async function run() {
         // make user admin
         app.put("/user/admin/:email", verifyJWT, async (req, res) => {
             const email = req.params.email;
-            const filter = { email: email };
-            const updateDoc = {
-                $set: { role: "admin" },
-            };
-            const result = await usersCollection.updateOne(filter, updateDoc);
-            res.send(result);
+            const requester = req.decoded.email;
+            const requesterAccount = await usersCollection.findOne({
+                email: requester,
+            });
+            if (requesterAccount.role === "admin") {
+                const filter = { email: email };
+                const updateDoc = {
+                    $set: { role: "admin" },
+                };
+                const result = await usersCollection.updateOne(
+                    filter,
+                    updateDoc
+                );
+                res.send(result);
+            } else {
+                res.status(403).send({ message: "Forbidden" });
+            }
+        });
+
+        // checking admin role
+        app.get("/admin/:email", async (req, res) => {
+            const email = req.params.email;
+            const user = await usersCollection.findOne({ email: email });
+            const isAdmin = user.role === "admin";
+            res.send({ admin: isAdmin });
         });
 
         // load all users
