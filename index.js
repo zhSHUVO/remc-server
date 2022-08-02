1a905f1f112a883bb1ad5f7d0ddcb96279c3099e
zhSHUVO <zhshuvo99@gmail.com>
2022-08-02 22:46:17 +0600

    update

--- index.js
+++ index.js
@@ -110,7 +110,7 @@ async function run() {
             res.send(users);
         });
 
-        // delete user 
+        // delete user
         app.delete("(/user/:id)", async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
